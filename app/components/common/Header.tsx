5933d923bec2e051c78d09de09905122c7aaa5e5
shafi <shafi.bahrami.2015@gmail.com>
2023-12-04 20:27:59 +0100

    header

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,7 +1,33 @@
+"use client"
+
+import Link from 'next/link';
+import {HiBars3, HiOutlineShoppingCart} from 'react-icons/hi2';
+
 const Header=()=>{
    return (
-      <header>
-         Header
+      <header className="grid grid-cols-2 py-5 px-4 md:px-12 items-center sticky top-0 z-10 bg-white">
+         {/*  left part */}
+         <div className="flex items-center gap-x-8">
+            <button className="p-2 rounded-full text-slate-200 text-gray-500 hover:bg-green-200 hover:text-green-600">
+               <HiBars3 size={28} className="cursor-pointer shrink-0"/>
+            </button>
+            <button>
+               Location
+            </button>
+         </div>
+         {/* right part */}
+         <div className="hidden md:flex justify-end space-x-4 items-center">
+            <Link href="/cart" className="relative p-2 bg-slate-200 rounded-full text-gray-500 hover:bg-green-200 hover:text-green-600">
+               <HiOutlineShoppingCart className="pr-1" size={28}/>
+               <span className='absolute top-0 right-1 text-green-600 font-bold'>
+                  0
+               </span>
+            </Link>
+            <button className='bg-slate-200 text-gray-500 px-4 py-1 rounded-full'>
+               Login/Signup
+            </button>
+
+         </div>
       </header>
    )
 }
