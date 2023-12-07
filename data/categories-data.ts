9403b1fcd4faa1024349d8af2752b865605979b0
shafi <shafi.bahrami.2015@gmail.com>
2023-12-07 20:39:53 +0100

    promos

--- data/categories-data.ts
+++ data/categories-data.ts
@@ -9,6 +9,7 @@ export const categoriesData = [
      desc: "Juicy, flavorful patties served in a bun with various toppings and condiments.",
      id: "cln3677fn00016uls4vp6dhx1",
      imageSrc: "/img/categories/pizza.png",
+     title:"Pizza"
    },
    {
      desc: "Italian classics featuring various pasta shapes paired with rich sauces and ingredients.",
