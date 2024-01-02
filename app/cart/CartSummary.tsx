307d00f628e82b16a0f17af353c180c4a94e7f20
shafi <shafi.bahrami.2015@gmail.com>
2024-01-02 22:38:17 +0100

    fixings

--- app/cart/CartSummary.tsx
+++ app/cart/CartSummary.tsx
@@ -113,7 +113,7 @@ const CartSummary = () => {
               <button
                 className=" py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
               >
-                CheckOut
+                Checkout
               </button>
             </div>
           </div>
