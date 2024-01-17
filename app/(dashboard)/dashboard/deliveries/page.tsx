f438bf77e0813c4cbcb45c8ab66eaad9e3901b25
shafi <shafi.bahrami.2015@gmail.com>
2024-01-17 19:06:52 +0100

    deliveries

---
+++ app/(dashboard)/dashboard/deliveries/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/app/components/common/Container";
+import DeliveriesTable from "./DeliveriesTable";
+
+const deliveriesPage=()=>{
+   return (
+      <Container>
+         <DeliveriesTable/>
+      </Container>
+   )
+}
+
+export default deliveriesPage;
