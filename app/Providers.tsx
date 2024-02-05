1190b46deb035cd22a0c552d3ddaf9c39b54a215
shafi <shafi.bahrami.2015@gmail.com>
2024-02-05 17:42:04 +0100

    adding providers

---
+++ app/Providers.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import AuthModal from "./components/common/AuthModal";
+
+const Providers=({children}:{children:ReactNode})=>{
+   return (
+      <div>
+         <AuthModal/>
+         {children}
+      </div>
+   )
+}
+
+export default Providers;
